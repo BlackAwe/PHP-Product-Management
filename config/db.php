df00b841c99feead27eb4ffcbcf0efdef3c270ba
Justin <itinjus@gmail.com>
2024-10-18 16:45:01 +0800

    First commit, setting up project directory

---
+++ config/db.php
@@ -0,0 +1,20 @@
+<?php
+
+function connectDB()
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "productdb";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    echo "<br>Connected successfully";
+    return $conn;
+}
